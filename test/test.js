19a1603ba0a5b26b75a3f2440d1109d61dddce26
Carlo Cervellin <carlocervellin@gmail.com>
2018-09-02 14:33:28 +0200

    Moved test files to indipendent file

--- test/test.js
+++ test/test.js
@@ -1,30 +1,19 @@
 const assert = require('assert');
 const PokerCore = require('../src/index');
 const verdictTests = require('./tests/verdictTests');
+const cardsStringTests = require('./tests/cardsStringTests');
 
 describe('Card', function() {
 	describe('cardFromString()', function() {
-		it('Should work', function() {
-			assert.equal(
-				PokerCore.cardFromString(' AD ').equals(new PokerCore.Card(1, 0)),
-				true
-			);
-			assert.equal(
-				PokerCore.cardFromString(' 2C').equals(new PokerCore.Card(0, 1)),
-				true
-			);
-			assert.equal(
-				PokerCore.cardFromString('7H ').equals(new PokerCore.Card(2, 6)),
-				true
-			);
-			assert.equal(
-				PokerCore.cardFromString('KD').equals(new PokerCore.Card(1, 12)),
-				true
-			);
-			assert.equal(
-				PokerCore.cardFromString('10D').equals(new PokerCore.Card(1, 9)),
-				true
-			);
+		cardsStringTests.forEach(test => {
+			it('Should work', function() {
+				assert.equal(
+					PokerCore.cardFromString(test.cardString).equals(
+						new PokerCore.Card(test.seedNumber, test.cardNumber)
+					),
+					true
+				);
+			});
 		});
 	});
 });
---
+++ test/tests/cardsStringTests.js
@@ -0,0 +1,27 @@
+module.exports = [
+	{
+		cardString: ' AD ',
+		seedNumber: 1,
+		cardNumber: 0
+	},
+	{
+		cardString: ' 2C',
+		seedNumber: 0,
+		cardNumber: 1
+	},
+	{
+		cardString: '7H ',
+		seedNumber: 2,
+		cardNumber: 6
+	},
+	{
+		cardString: 'KD',
+		seedNumber: 1,
+		cardNumber: 12
+	},
+	{
+		cardString: '10D',
+		seedNumber: 1,
+		cardNumber: 9
+	}
+];
