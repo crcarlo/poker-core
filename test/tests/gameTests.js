68eff94bac1e58e1fcfb32dcf1c98c5ef139b4fe
Carlo Cervellin <carlocervellin@gmail.com>
2018-09-04 22:52:50 +0200

    Added more tests

--- test/tests/gameTests.js
+++ test/tests/gameTests.js
@@ -4,5 +4,65 @@ module.exports = [
 		table: '3H KH 7D',
 		tableCards: ['9D', '10C'],
 		result: [1]
+	},
+	{
+		hands: ['10H 5D', '6C 4D'],
+		table: 'QC 2S QD',
+		tableCards: ['QH', 'JS'],
+		result: [0]
+	},
+	{
+		hands: ['10D 7S', 'JS 4C'],
+		table: '4H 10S 6D',
+		tableCards: ['9S', '8D'],
+		result: [0]
+	},
+	{
+		hands: ['9C 9H', '6S KS'],
+		table: '5C 3H 9S',
+		tableCards: ['10S', 'AD'],
+		result: [0]
+	},
+	{
+		hands: ['3C JC', '10H KD'],
+		table: '9C KC QD',
+		tableCards: ['8D', '6H'],
+		result: [1]
+	},
+	{
+		hands: ['2S 5D', '4S 2H'],
+		table: '7C 10H JC',
+		tableCards: ['8C', 'KH'],
+		result: [0, 1]
+	},
+	{
+		hands: ['5C 10C', '2H 8H'],
+		table: '9S 5D KH',
+		tableCards: ['9C', 'AH'],
+		result: [0]
+	},
+	{
+		hands: ['7H JH', '3C AS'],
+		table: '6D QH 6H',
+		tableCards: ['4C', 'KD'],
+		result: [1]
+	},
+	{
+		hands: ['5S AC', 'JH QS'],
+		table: 'AD 10D 2H',
+		tableCards: ['QC', '6D'],
+		result: [0]
+	},
+	{
+		hands: ['5C KD', '6D 5H'],
+		table: '8C 2H 10S',
+		tableCards: ['7C', '4D'],
+		result: [1]
+	},
+	{
+		hands: ['QH QC', '5S 5C'],
+		table: '8C 2D KC',
+		tableCards: ['9D', 'KS'],
+		result: [0]
 	}
 ];
